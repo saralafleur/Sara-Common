941d99f3491e4f760654e7df40593cbea2cae514
Sara LaFleur <saralafleur47@gmail.com>
2019-11-04 16:51:24 -0700

    Shoot.  These files were ignored for some reason.

---
+++ Sara.Common/Debug/Shutdown.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+
+namespace Sara.Common.Debug
+{
+    /// <summary>
+    /// Global Thread safe class to signal a process to Shutdown
+    /// </summary>
+    public static class Shutdown
+    {
+        private static readonly ManualResetEvent _shutdown = new ManualResetEvent(false);
+        public static bool Now => _shutdown.WaitOne(0);
+
+        public static WaitHandle WaitHandle => _shutdown;
+
+        public static void Set()
+        {
+            _shutdown.Set();
+        }
+    }
+}
---
+++ Sara.Common/Debug/Trace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Sara.Common.Debug
+{
+    /// <summary>
+    /// Simple class used to trace the duration of an activity in Milliseconds
+    /// Returns the Total and AverageDuration of an activity
+    /// </summary>
+    public class Trace
+    {
+        public int TotalDurationMs;
+        public int AverageDurationMs;
+        private int _current;
+        public void Add(System.DateTime start)
+        {
+            Interlocked.Increment(ref _current);
+            var durationMs = (int)Math.Round((System.DateTime.Now - start).TotalMilliseconds);
+            Interlocked.Add(ref TotalDurationMs, durationMs);
+            Interlocked.Exchange(ref AverageDurationMs, TotalDurationMs / _current);
+        }
+    }
+}
